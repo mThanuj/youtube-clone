df593f37b8e52e1fa285fcab1c4e79a2a658ac38
mthanuj <mullagurithanuj0@gmail.com>
2025-03-06 08:29:56 +0530

    sidebar layout done

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 const Page = () => {
-  return (
-    <div>
-      <Image src="/logo.svg" width={50} height={50} alt="logo" />
-      <p className="text-xl font-semibold tracking-tight">NewTube</p>
-    </div>
-  );
+  return <div>I will load videos in the future</div>;
 };
 
 export default Page;
